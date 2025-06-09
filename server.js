036e42a3a92ce78ce36d6f4f2bb82b5d126f86d2
Fadilla Ma'sum Haqqoni <fadillamaksum650@gmail.com>
2025-06-09 18:21:26 +0700

    nambahin PORT

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import Hapi from "@hapi/hapi";
 import routes from "./src/routes/routes.js";
 import connectDB from "./src/config/mongoose.js";
 import cookie from "@hapi/cookie";
+import dotenv from "dotenv";
+dotenv.config();
 
 const init = async () => {
   await connectDB();
   console.log("Koneksi ke MongoDB berhasil");
   const server = Hapi.server({
-    port: 9000,
-    host: "localhost",
+    port: process.env.PORT || 4000,
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
