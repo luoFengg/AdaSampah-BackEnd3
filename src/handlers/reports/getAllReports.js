a0e4a6a2dfe192850a244c16c380add9e81e76ff
Fadilla Ma'sum Haqqoni <fadillamaksum650@gmail.com>
2025-06-10 17:54:24 +0700

    ngubah urutan penampilan reports biar yang terbaru jadi paling awal

--- src/handlers/reports/getAllReports.js
+++ src/handlers/reports/getAllReports.js
@@ -2,11 +2,13 @@ import Report from "../../models/reportSchema.js";
 
 const getAllReports = async (request, h) => {
   try {
-    const reports = await Report.find({});
+    const reports = await Report.find({}).sort({ createdAt: -1 });
     return h.response({ status: "success", data: reports }).code(200);
   } catch (error) {
     console.error("Error fetching reports:", error);
-    return h.response({ status: "fail", message: "Failed to fetch reports" }).code(500);
+    return h
+      .response({ status: "fail", message: "Failed to fetch reports" })
+      .code(500);
   }
 };
 
--- src/handlers/reports/getAllReportsPaginate.js
+++ src/handlers/reports/getAllReportsPaginate.js
@@ -5,7 +5,10 @@ const getAllReportsPaginate = async (request, h) => {
   try {
     const { page = 1, limit = 9 } = request.query;
     const skip = (page - 1) * limit;
-    const reports = await Report.find({}).skip(skip).limit(parseInt(limit));
+    const reports = await Report.find({})
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(parseInt(limit));
     const total = await Report.countDocuments();
 
     return h
@@ -18,7 +21,9 @@ const getAllReportsPaginate = async (request, h) => {
       .code(200);
   } catch (error) {
     console.error("Error fetching reports:", error);
-    return h.response({ status: "fail", message: "Failed to fetch reports" }).code(500);
+    return h
+      .response({ status: "fail", message: "Failed to fetch reports" })
+      .code(500);
   }
 };
 
